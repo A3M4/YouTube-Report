3097a2dfc8fe1979daccb1713ec252bda409e97b
Riedler <david.riedler@gelse.net>
2019-12-28 15:14:17 +0100

    fixed watchetime heatmap

--- parse.py
+++ parse.py
@@ -76,22 +76,18 @@ class HTML:
 
     def find_times(self):
         times = []
-        pattern = re.compile(r'\d{1,2}\s.{9}\s\d\d:\d\d:\d\d\sEST')
+        for translation in (r"""\xa0watched<br><a href=\"[^\"]*\">[^<]*<\/a><br>(\d\d?)\.(\d\d?)\.(\d\d\d\d), (\d\d?):(\d\d?):(\d\d?) ([^<]*)<\/div>""",r"""\xa0angesehen<br><a href=\"[^\"]*\">[^<]*<\/a><br>(\d\d?)\.(\d\d?)\.(\d\d\d\d), (\d\d?):(\d\d?):(\d\d?) ([^<]*)<\/div>"""):
+        	times+=self.raw_find_times(translation)
+        return times
+        
+    def raw_find_times(self,translation):
+        pattern = re.compile(translation)
         matchList = pattern.findall(str(self.htmlWatch))
-
+        times=[]
         # add '0' to the beginning of the string to make all string same length
         for time in matchList:
-            if len(time) == 24:
-                time = str(0) + time
-                # add the day of week to the end of strings
-                dayOfWeek = datetime.datetime.strptime(time[0:11], '%d %b %Y').strftime('%a')
-                times.append(time + ' ' + dayOfWeek)
-            else:
-                # add the day of week to the end of strings
-                dayOfWeek = datetime.datetime.strptime(time[0:11], '%d %b %Y').strftime('%a')
-                times.append(time + ' ' + dayOfWeek)
+            times.append(datetime.datetime.strptime("%s.%s.%s %s:%s:%s %s"%(time),'%d.%m.%Y %H:%M:%S %Z'))
         return times
-
 
 
     def searchHistory(self):
@@ -134,71 +130,14 @@ class HTML:
 
 
     def dataframe_heatmap(self,day):
-        timeWeeks = []
-        daytime = []
         times = self.find_times()
+        watchtimes=[0 for t in range(12)]
+        
         for time in times:
-            timeWeek = time[-3:]+time[13:15]
-            timeWeeks.append(timeWeek)
-        freq = collections.Counter(timeWeeks)
-        for k, v in freq.items():
-            if k[0:3] == day:
-                daytime.append(str(k)+' '+str(v))
-        daytime.sort(key=lambda x: int(str(x)[3:5]))
-        print(daytime)
+        	if time.weekday()==day:
+        		watchtimes[(time.hour//2)-time.hour%2]+=1
 
-        zero_one = 0
-        two_three = 0
-        four_five = 0
-        six_seven= 0
-        eight_nine = 0
-        ten_eleven = 0
-        twelve_thirteen = 0
-        fourteen_fifteen = 0
-        sixteen_seventeen = 0
-        eighteen_nineteen = 0
-        twenty_twentyone = 0
-        twentytwo_twentythree = 0
-
-        for i in daytime:
-            if int(i[3:5]) in range(0, 2):
-                zero_one = zero_one + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(2, 4):
-                two_three = two_three + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(4, 6):
-                four_five = four_five + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(6, 8):
-                six_seven = six_seven + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(8, 10):
-                eight_nine = eight_nine + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(10, 12):
-                ten_eleven = ten_eleven + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(12, 14):
-                twelve_thirteen = twelve_thirteen + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(14, 16):
-                fourteen_fifteen = fourteen_fifteen + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(16, 18):
-                sixteen_seventeen = sixteen_seventeen + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(18, 20):
-                eighteen_nineteen = eighteen_nineteen + int(i.split(' ')[1])
-            elif int(i[3:5]) in range(20, 22):
-                twenty_twentyone = twenty_twentyone + int(i.split(' ')[1])
-            else:
-                twentytwo_twentythree = twentytwo_twentythree + int(i.split(' ')[1])
-
-        return ([zero_one, two_three, four_five, six_seven, eight_nine, ten_eleven, twelve_thirteen, fourteen_fifteen,
-                 sixteen_seventeen, eighteen_nineteen, twenty_twentyone, twentytwo_twentythree])
-
-
-
-
-
-
-
-
-
-
-
+        return watchtimes
 
 
 
